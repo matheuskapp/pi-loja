4dc605c11dab60befda165038c19bcc91d7da903
beatriztrm <beatriztrudes98@gmail.com>
2026-02-27 19:29:46 -0300

    atualização menu lateral

---
+++ app/cadastro_clientes/menu.lateral.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./cadastro_clientes.css"
+export default function CadastroClientes() {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
