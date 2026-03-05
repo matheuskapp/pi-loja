fd194ca17d40c8e03f995020d1364803bb7d703f
Natanaelv7 <nattanaell10@gmail.com>
2026-03-05 20:38:26 -0300

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -8,39 +8,32 @@ export default function dashboard() {
   return (
 
 
-    <div className="row">
-
-        <div className="col-2">
-
-          <BarraLateral />
-
-        </div>
-
-        <div className="col-10 p-4">
-
-          
-          <div className="d-flex justify-content-between align-items-center mb-4">
-
-            <div>
-              <h2 className="fw-bold mb-1">Dashboard</h2>
-              <small className="text-muted">
-                Visão geral do seu negócio • 26/02/2026
-              </small>
-            </div>
-
-            <button className="btn btn-dark d-flex align-items-center gap-2 px-4 py-2">
-              <i className="bi bi-cart3"></i>
-            🛒 Nova Venda
-            </button>
-
-          </div>
-
-          <DashBoard />
-
-        </div>
-
+    <div style={{ display: "flex" }}>
+      
+      <BarraLateral />
 
       
+      <main style={{ marginLeft: "260px", width: "100%", padding: "24px" }}>
+        
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <div>
+            <h2 className="fw-bold mb-1">Dashboard</h2>
+            <small className="text-muted">
+              Visão geral do seu negócio • 26/02/2026
+            </small>
+          </div>
+
+
+          <button className="btn btn-dark d-flex align-items-center gap-2 px-4 py-2">
+            <i className="bi bi-cart3"></i>
+            🛒 Nova Venda
+          </button>
+
+
+        </div>
+
+        <DashBoard />
+      </main>
     </div>
   );
 }
