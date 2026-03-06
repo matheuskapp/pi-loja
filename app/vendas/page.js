7a7a6a8e62fa962891cec05729973811a60f7131
Natanaelv7 <nattanaell10@gmail.com>
2026-03-05 21:01:48 -0300

    upload

--- app/vendas/page.js
+++ app/vendas/page.js
@@ -3,15 +3,25 @@ import Vendas from "./Vendas";
 import BarraLateral from "../components/barra_lateral";
 
 export default function vendas() {
-    return(
-        <div class = "row" >
+    return (
+    <div style={{ display: "flex" }}>
+      
+      <BarraLateral />
 
-            <div className="col-2" >
-
-                <BarraLateral/>
-
-            </div>
-            
+      
+      <main style={{ marginLeft: "260px", width: "100%", padding: "24px" }}>
+        
+        
+        <div className="mb-4">
+          <h2 className="fw-bold">Gestão de Vendas</h2>
+          <p className="text-muted">Cadastre e gerencie as vendas do sistema Boy+ Plus</p>
+          <hr />
         </div>
-    )
+
+        
+        <Vendas />
+
+      </main>
+    </div>
+  );
 }
