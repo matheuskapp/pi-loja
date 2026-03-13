51d4e06e11632cb0babb0d88e9342d6f84854dff
beatriztrm <beatriztrudes98@gmail.com>
2026-03-13 19:29:15 -0300

    alterações - async / alert

--- app/cadastro_clientes/page.js
+++ app/cadastro_clientes/page.js
@@ -16,89 +16,128 @@ export default function CadastroClientes() {
 
     const [listaClientes, alteraListaClientes] = useState([]);
 
-    function salvar(e) {
-        e.preventDefault()
+    async function cancelar() {
+        const { data, error } = await supabase
+            .from('cadastro_clientes')
+            .select()
+        console.log(data)
+        alteraListaClientes(data)
+    }
+
+    async function salvar() {
+
         const objeto = {
             nome: nome,
-            cpf: cpf,
+            cpf: cpf.replaceAll(".", ""),
             telefone: telefone,
             email: email,
             endereco: endereco,
-            
+
 
         }
-        console.log(objeto)
+
+        function salvar(e) {
+            e.preventDefault()
+            const objeto = {
+                nome: nome,
+                cpf: cpf,
+                telefone: telefone,
+                email: email,
+                endereco: endereco,
+
+
+            }
+            console.log(objeto)
+        }
+
+         const { error } = await supabase
+            .from('cadastro_clientes')
+            .insert(objeto)
+
+        console.log(error)
+
+        if(error == null){
+            alert("Cliente cadastrado com sucesso!")
+            alteraNome("")
+            alteraCpf("")
+            alteraTelefone()
+            alteraEmail("")
+            alteraEndereco("")
+
+        }else alert("Dados inválidos, verifique os campos e tente novamente...")
+
     }
-    return (
-        <div className="row">
-            <div className='col-2'>
 
-                <BarraLateral />
+        return (
+            <div className="row">
+                <div className='col-2'>
 
-            </div>
-            <div className="col-9">
+                    <BarraLateral />
 
-                <div className="container-fluid" />
-                <h1>Cadastro de Clientes</h1>
+                </div>
+                <div className="col-9">
+
+                    <div className="container-fluid" />
+                    <h1>Cadastro de Clientes</h1>
 
 
 
 
-                <div className="titulo" >
+                    <div className="titulo" >
+
+                    </div>
+
+
+                    <form onsubmit="salvar(event)">
+
+
+                        <br /> <br />
+
+                        <form>
+                            <div class="nomeCompleto">
+                                <label class="form-label">Nome Completo</label>
+                                <input onChange={e => alteraNome(e.target.value)} type="text" class="form-control" />
+
+                            </div>
+                            <div class="cpf">
+                                <label class="form-label">CPF</label>
+                                <input onChange={e => alteraCpf(e.target.value)} type="number" class="form-control" />
+                            </div>
+                            <br />
+                            <div class="telefone">
+                                <label class="form-label">Telefone</label>
+                                <input onChange={e => alteraTelefone(e.target.value)} type="number" class="form-control" />
+                            </div>
+                            <br />
+                            <div class="emailSenha">
+                                <label class="form-label">E-mail</label>
+                                <input onChange={e => alteraEmail(e.target.value)} type="email" class="form-control" />
+                            </div>
+                            <br />
+                            <div class="endereco">
+                                <label class="form-label">Endereço</label>
+                                <input onChange={e => alteraEndereco(e.target.value)} class="form-control" />
+                            </div>
+                            <br />
+
+
+
+                            <button onClick={salvar} type="Salvar" class="btn btn-primary me-3">Salvar</button>
+                            <button onClick={cancelar} type="button" class="btn btn-dark">Cancelar</button>
+
+                        </form>
+
+                        <br /><br />
+
+                    </form>
 
                 </div>
 
 
-                <form onsubmit="salvar(event)">
 
-
-                    <br /> <br />
-
-                    <form>
-                        <div class="nomeCompleto">
-                            <label class="form-label">Nome Completo</label>
-                            <input onChange={e => alteraNome(e.target.value)} type="text" class="form-control" />
-
-                        </div>
-                        <div class="cpf">
-                            <label class="form-label">CPF</label>
-                            <input onChange={e => alteraCpf(e.target.value)} type="number" class="form-control" />
-                        </div>
-                        <br />
-                        <div class="telefone">
-                            <label class="form-label">Telefone</label>
-                            <input onChange={e => alteraTelefone(e.target.value)} type="number" class="form-control" />
-                        </div>
-                        <br />
-                        <div class="emailSenha">
-                            <label class="form-label">E-mail</label>
-                            <input onChange={e => alteraEmail(e.target.value)} type="email" class="form-control" />
-                        </div>
-                        <br />
-                        <div class="endereco">
-                            <label class="form-label">Endereço</label>
-                            <input onChange={e => alteraEndereco(e.target.value)} class="form-control" />
-                        </div>
-                        <br />
-
-
-
-                        <button onClick={salvar} type="Salvar" class="btn btn-primary me-3">Salvar</button>
-                        <button type="button" class="btn btn-dark">Cancelar</button>
-
-                    </form>
-
-                    <br /><br />
-
-                </form>
 
             </div>
 
 
-
-
-        </div>
-
-
-    )
-}
+        )
+    }
