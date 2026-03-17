2c7a96c0242b6999c605a72d1a0f92793d9271ff
beatriztrm <beatriztrudes98@gmail.com>
2026-03-17 18:48:36 -0300

    criação pasta conexao - supabase

---
+++ app/conexao/supabase.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://walrpbrbskwawykdrwna.supabase.co', 'sb_publishable_e6ELe320z3xGITtIk-2QVg_K3tLxQTa')
+export default supabase
