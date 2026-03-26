f832ad26f24acb9fd4b0ead51307e028fde26a2b
StephanieFranciele <stephaniefranciele2011@gmail.com>
2026-03-25 21:51:40 -0300

    inclui o botão editar

--- app/cadastro_funcionarios/listafuncionarios.js
+++ app/cadastro_funcionarios/listafuncionarios.js
@@ -1,53 +1,80 @@
 'use client';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import supabase from "../conexao/supabase";
 
-export function ListaFuncionarios() {
-
-    const [listafuncionarios, alteraListaFuncionarios] = useState([])
-
-  
+export function ListaFuncionarios({ listafuncionarios, alteraListaFuncionarios }) {
+   
+    const [nome, alteraNome] = useState("")
+    const [email, alteraEmail] = useState("")
+    const [senha, alteraSenha] = useState("")
+    const [editando, alteraEditando] = useState(null)
 
     async function buscar() {
         const { data, error } = await supabase
             .from('usuarios')
             .select()
+
         console.log(data)
+        console.log(error)
+
         alteraListaFuncionarios(data)
     }
 
-     async function Excluir(id) {
+    async function salvar(e) {
+        e.preventDefault()
 
-        const opcao = confirm("Tem certeza que deseja excluir?")
-       if(opcao == false){
-        return
-       }
-       const response = await supabase.from ('usuarios').delete().eq('id',id)
+        const objeto = {
+            nome: nome,
+            email: email,
+            senha: senha
+        }
 
+        const { error } = await supabase
+            .from('usuarios')
+            .update(objeto)
+            .eq('id', editando)
+
+        console.log(error)
+
+        if (error == null) {
+            alert("Atualização realizada com sucesso")
+            cancelaEdicao()
+            buscar()
+        } else {
+            alert("Verifique os campos e tente novamente!")
+        }
     }
 
+    function editar(objeto) {
+        alteraEditando(objeto.id)
+        alteraNome(objeto.nome)
+        alteraEmail(objeto.email)
+        alteraSenha(objeto.senha)
+    }
 
-    useEffect(() => {
-        buscar()
-    }, [])
-
-   
+    function cancelaEdicao() {
+        alteraEditando(null)
+        alteraNome("")
+        alteraEmail("")
+        alteraSenha("")
+    }
 
     return (
         <div>
 
-            <div class="container py-5 bg-light text-align-left ms-4 rounded-5">
-                <div class="col text-end">
-                    <button onClick={() => buscar()} class="btn btn-primary p-1 mb-2">🔄</button>
+            <div className="container py-5 bg-light text-align-left ms-4 rounded-5">
+                <div className="col text-end">
+                    <button onClick={buscar} className="btn btn-primary p-1 mb-2">🔄</button>
                 </div>
-                <div class="card shadow-sm border-0 rounded-4">
-                    <div class="card-body">
-                    
-                        <table class="table align-middle">
-                            <thead class="text-muted">
+
+                <div className="card shadow-sm border-0 rounded-4">
+                    <div className="card-body">
+
+                        <table className="table align-middle">
+                            <thead className="text-muted">
                                 <tr>
-                                    <th >#</th>
-                                    <th >Nome</th>
+                                    <th>#</th>
+                                    <th>Nome</th>
                                     <th>Email</th>
                                     <th>Senha</th>
                                     <th className="text-end">Ações</th>
@@ -55,29 +82,125 @@ export function ListaFuncionarios() {
                             </thead>
 
                             <tbody>
-
                                 {
-                                    listafuncionarios.map(
-                                        (item, index) => <tr key={index}>
+                                    listafuncionarios.map((item, index) => (
+                                        <tr key={index}>
                                             <td>{item.id}</td>
                                             <td>{item.nome}</td>
                                             <td>{item.email}</td>
                                             <td>****</td>
 
-                                            <td class="text-end">
-                                                <button class="btn btn-primary btn-sm me-2">Editar</button>
-                                                                                                <button onClick={() => { Excluir (item.id); buscar() }} class="btn btn-danger btn-sm">Excluir</button>
+                                            <td className="text-end">
+                                                <button 
+                                                    onClick={() => editar(item)} 
+                                                    className="btn btn-primary btn-sm me-2"
+                                                    data-bs-target="#modaledicao" 
+                                                    data-bs-toggle="modal" 
+                                                >
+                                                    Editar
+                                                </button>
+
+                                                <button 
+                                                    className="btn btn-danger btn-sm"
+                                                >
+                                                    Excluir
+                                                </button>
                                             </td>
                                         </tr>
-
-                                    )
+                                    ))
                                 }
                             </tbody>
+
                         </table>
 
                     </div>
                 </div>
             </div>
+
+            <div
+                className="modal fade"
+                id="modaledicao"
+                tabIndex="-1"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+
+                        <div className="modal-header">
+                            <h3 className="modal-title fs-5">
+                                Edição de Produto
+                            </h3>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+
+                        <div className="modal-body">
+
+                            <div className="mb-3">
+                                <label className="form-label w-100">
+                                    <input 
+                                        value={nome} 
+                                        onChange={e => alteraNome(e.target.value)}
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Nome"
+                                    />
+                                </label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label className="form-label w-100">
+                                    <input 
+                                        value={email} 
+                                        onChange={e => alteraEmail(e.target.value)}
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Email"
+                                    />
+                                </label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label className="form-label w-100">
+                                    <input 
+                                        value={senha} 
+                                        onChange={e => alteraSenha(e.target.value)}
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Senha"
+                                    />
+                                </label>
+                            </div>
+
+                        </div>
+
+                        <div className="modal-footer">
+                            <button 
+                                onClick={cancelaEdicao}
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Fechar
+                            </button>
+
+                            <button
+                                onClick={salvar}
+                                className="btn btn-primary"
+                                data-bs-dismiss="modal"
+                            >
+                                Salvar
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
